e49df3de619111c7f1494dcc039de69c3e9ae8b9
machetie <machetie@users.noreply.github.com>
2026-02-09 03:29:17 +1100

    tweak: reorder z-order so bongo cat and output status render above layer roller, move bongo cat to top-left corner

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -59,6 +59,17 @@ lv_obj_t *zmk_display_status_screen()
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
+#if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
+#if CONFIG_DONGLE_SCREEN_LAYER_ROLLER
+    zmk_widget_layer_roller_init(&layer_roller_widget, screen);
+    lv_obj_set_size(zmk_widget_layer_roller_obj(&layer_roller_widget), 224, 140);
+    lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_LEFT_MID, 0, -20);
+#else
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, 0);
+#endif
+#endif
+
 #if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
     zmk_widget_output_status_init(&output_status_widget, screen);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 10);
@@ -71,21 +82,10 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_BONGO_CAT
     zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_TOP_LEFT, 20, 10);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 #elif CONFIG_DONGLE_SCREEN_WPM_ACTIVE
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
     lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_LEFT, 20, 20);
-#endif
-
-#if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
-#if CONFIG_DONGLE_SCREEN_LAYER_ROLLER
-    zmk_widget_layer_roller_init(&layer_roller_widget, screen);
-    lv_obj_set_size(zmk_widget_layer_roller_obj(&layer_roller_widget), 224, 140);
-    lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_LEFT_MID, 0, -20);
-#else
-    zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, 0);
-#endif
 #endif
 
 #if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
